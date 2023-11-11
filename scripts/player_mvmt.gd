20f04b573663bcbbe2492cae088c159c75395a73
ratboyblue <100643800+ratboyblue@users.noreply.github.com>
2023-11-11 15:44:55 -0500

    added wall jump but it sucks

--- scripts/player_mvmt.gd
+++ scripts/player_mvmt.gd
@@ -7,8 +7,10 @@ var acceleration = 0.7;
 var air_acceleration = 0.35;
 var friction = 0.15;
 
-var double_jump = false;
-var double_jump_velocity = jump_velocity * .5;
+var can_double_jump = true;
+var double_jump_velocity = jump_velocity * 0.65;
+
+var can_wall_jump = true;
 
 #inventory variables
 var has_key = 0;
@@ -24,14 +26,16 @@ func _physics_process(delta):
 
 	#jump from floor
 	if is_on_floor():
-		double_jump = false;
+		can_double_jump = true;
+		can_wall_jump = true;
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = jump_velocity;
 	#double jump
-	if not is_on_floor() && double_jump == false:
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = double_jump_velocity;
-			double_jump = true;
+	double_jump();
+	
+	#wall jump
+	wall_jump();
+	
 
 	#get input direction and apply acceleration or friction
 	var dir = Input.get_axis("ui_left", "ui_right")
@@ -44,3 +48,17 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, 0.0, friction);
 
 	move_and_slide();
+	
+func double_jump():
+	if not is_on_floor() && can_double_jump == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = double_jump_velocity;
+			can_double_jump = false;
+			
+func wall_jump():
+	var wall_normal = get_wall_normal();
+	if is_on_wall() && can_wall_jump == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.x = wall_normal.x * speed * 2;
+			velocity.y = jump_velocity;
+			can_wall_jump = false;
