8d22e7cccecf659586d878c37b1d3938f3c2062a
ratboyblue <100643800+ratboyblue@users.noreply.github.com>
2023-11-10 20:40:49 -0500

    add camera

--- scenes/player_mvmt.gd
+++ scenes/player_mvmt.gd
@@ -19,9 +19,9 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var dir = Input.get_axis("ui_left", "ui_right")
+	if dir:
+		velocity.x = dir * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
