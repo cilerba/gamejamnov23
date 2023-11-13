9207bd884323685ab7dc7aec82099924a7303488
ratboyblue <100643800+ratboyblue@users.noreply.github.com>
2023-11-12 21:36:27 -0500

    health_up

--- scripts/health_up.gd
+++ scripts/health_up.gd
@@ -5,19 +5,13 @@ var following_body;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	body_entered.connect(on_body_enter);
-	GameManager.hide_key.connect(hide_key)
 	
 func on_body_enter(body):
-	if (body.get_name() == "Player" && !following_body):
-		GameManager.keys += 1
+	if (body.get_name() == "Player") && GameManager.health < GameManager.MAX_HEARTS:
+		GameManager.health += 1;
 		GameManager.play("res://sounds/getcrystal.wav")
-		following_body = body
-	
+		queue_free();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (following_body):
-		position = lerp(position, following_body.position + Vector2(16 * (1 if following_body.sprite.flip_h else -1), 5), delta * 20)
-
-func hide_key():
-	queue_free()
+	pass;
---
+++ scripts/room_codex.gd
@@ -0,0 +1,19 @@
+extends Node
+#Room Order
+#Title
+#A: room_8
+#B: room_9
+#C: room_3
+#D: room_10
+#F: room_12
+#G: room_14
+#H: room_7
+#I: room_13
+#J: 
+#K: room_11
+#L
+#M
+#N
+#O
+#P
+#Win!
